66e65670ac0879d2df8fe8a8d0cd7ca4263185ac
Max Zykin <sanchez.max@yandex.ru>
2023-12-29 10:56:37 +0400

    done

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Aleksey Novikov on 04.08.2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
